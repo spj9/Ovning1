85d505465197440d28d01dec53f79bbfda77f6c6
spj9 <sanny_j@hotmail.com>
2024-05-20 16:10:34 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning1
+{
+    public class Employee
+    {
+            public string name;
+            public string salary;
+
+            public Employee(string inputName, string inputSalary) 
+            { 
+                name = inputName;
+                salary = inputSalary;   
+            }
+        public void DisplayRegisteredEmployee()
+        {
+            Console.WriteLine("Namn: " + name + "\t Lön: " + salary);
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Övning 1 i C#
+
+using Ovning1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Registrera ny anställd");
+        Console.WriteLine("Ange namn:");
+
+        string inputName = Console.ReadLine();
+
+        Console.WriteLine("Ange lön:");
+
+        string inputSalary = Console.ReadLine();
+
+        Employee person = new Employee(inputName, inputSalary);
+
+        person.DisplayRegisteredEmployee();
+    }
+}
